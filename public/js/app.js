96b180d4db9268e7cc5d17c6b091b168ce74940c
Adam Youngblood <ayoungbl0d@gmail.com>
2016-10-20 20:40:36 -0500

    Starter

---
+++ public/js/app.js
@@ -0,0 +1,34 @@
+(function(){
+  angular.module('ProfileSite', ['ui.router'])
+    .config(MainRouter);
+
+    MainRouter.$inject = ['$stateProvider', '$urlRouterProvider', '$locationProvider'];
+
+    function MainRouter($stateProvider, $urlRouterProvider, $locationProvider) {
+
+      $urlRouterProvider.otherwise("/home");
+
+      $stateProvider
+        .state('home', {
+          url: '/home',
+          templateUrl: 'home.html'
+        })
+        .state('projects', {
+          url: '/projects',
+          templateUrl: 'projects.html'
+        })
+        .state('contact', {
+          url: '/contact',
+          templateUrl: 'contact.html'
+        })
+        .state('about', {
+          url: '/about',
+          templateUrl: 'about.html'
+        })
+
+        $locationProvider.html5Mode({
+          enabled: true,
+          requireBase: false
+        });
+    }
+})()
---
+++ public/js/mainController.js
@@ -0,0 +1,13 @@
+(function(){
+  angular.module('ProfileSite')
+    .controller('MainController', MainController);
+
+    function MainController(){
+
+      var self = this;
+
+      self.hi = "Hello";
+      
+    }
+
+})()
