60b03437bf6d81ffb560a4d6c8fe0f91867f2912
Adam Youngblood <ayoungbl0d@gmail.com>
2016-11-02 19:55:37 -0500

    favicon

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 var express = require('express');
 var app     = express();
 var path    = require('path');
+var port    = process.env.PORT || 3000
 
 app.use(express.static(path.join(__dirname,'public')));
 
-app.get('/', function(req, res){
-    res.render('index');
+app.use('/scripts', express.static(__dirname + '/bower_components'))
+
+app.use(favicon(__dirname + '/public/favicon.ico'));
+
+app.all('/*', function(req, res, next) {
+  res.sendFile('/public/index.html', { root: __dirname });
 });
 
-app.listen(process.env.PORT, function(){
+app.listen(port, function(){
   console.log("++++++++++++++++++++++++++++++++++")
-  console.log("Node running on port "+process.env.PORT);
+  console.log("Node running on port "+port);
   console.log("++++++++++++++++++++++++++++++++++")
 });
