529709331676b8d9d265387f377a7d252095e57f
Adam Youngblood <ayoungbl0d@gmail.com>
2016-12-02 15:41:05 -0600

    info for ssl

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ app.use(express.static(path.join(__dirname,'public')));
 
 app.use('/scripts', express.static(__dirname + '/bower_components'))
 
+app.get('/.well-known/acme-challenge/3dXFJ_srhvtoggb08zRFYfqY2VTyhQODnmvOn6Xx9dc', function(req, res){
+res.send("3dXFJ_srhvtoggb08zRFYfqY2VTyhQODnmvOn6Xx9dc.22EMFKUX8piP_SxRePAiQWKA8lAly-1jhmIggcizTfs")
+});
+
 app.all('/*', function(req, res, next) {
   res.sendFile('/public/index.html', { root: __dirname });
 });
